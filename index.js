5176d960664bb1f5af912d5c13401da523e91c04
Galanight <43757205+Galanight@users.noreply.github.com>
2018-10-01 20:53:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const bot = new Discord.Client();
 const cfg require('./index.json'); // a garder en version desktop
 const token = process.env.token // a garder en version heroku
 bot.on('ready' => {
-    console.log("Bot Ready !¨);
-                });
-const prefix = ("?");
+console.log("Bot Ready !¨)
+   });
+
+    const prefix = ("?");
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('rien').catch(console.error)
